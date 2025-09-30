12bae5799016acf17d66d4da8904383db0ec301f
Leonardo-Garzon-1995 <lgarzonlc@gmail.com>
2025-09-30 06:32:58 +0200

    add boilerplate object for teams

---
+++ script.js
@@ -0,0 +1,132 @@
+const rugbyTeams ={
+    "England": {
+        headCoach: "Stuart Hogg",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Ireland": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Scotland": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Wales": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "France": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Italy": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Spain": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Australia": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "New Zealand": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+    "Argentina": {
+        headCoach: "John Anderson",
+        year: 2023,
+        victories: 6,
+        players: [
+            {
+                name: "Tom Lawrence",
+                number: 1,
+                position: "Fly Half",
+                image: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+            },
+        ]
+    },
+}
